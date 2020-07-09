a41455ca21fe992becc18eeff89ee9a7c031d6af
Leo <leopard20@users.noreply.github.com>
2020-07-10 02:12:51 +0430

    Add files via upload

---
+++ mod.cpp
@@ -0,0 +1,8 @@
+name = "Anti-Bounce System";
+author = "Leopard20";
+picture = "ABS.paa";
+logo = "ABS.paa";
+description = "Anti-Bounce System by Leopard20";
+overview = "Anti-Bounce System by Leopard20";
+overviewText = "Anti-Bounce System by Leopard20";
+overviewFootnote = "";
